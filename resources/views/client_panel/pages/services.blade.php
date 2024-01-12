0d761fe29daa54169d4e6dfe0145819855e3b4a4
ti7112 <ti7112@outlook.com>
2024-01-12 12:48:55 +0530

    updated_124712012024

--- resources/views/client_panel/pages/services.blade.php
+++ resources/views/client_panel/pages/services.blade.php
@@ -4,7 +4,24 @@
 
 @section('content')
 
-About page
+    {{-- <div class="w-full flex justify-center">
+        <div class="flex w-[80%] bg-white ">
+            <div class="w-1/2">
+                <p>No. 1 Ev service provider</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus officiis animi dolore eius ipsum, ab
+                    maiores aspernatur, incidunt vero mollitia id labore tempora, laudantium ullam? Laborum inventore
+                    tenetur aperiam odit.
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus officiis animi dolore eius ipsum, ab
+                    maiores aspernatur, incidunt vero mollitia id labore tempora, laudantium ullam? Laborum inventore
+                    tenetur aperiam odit.</p>
+                    
+            </div>
+            <div class="w-1/2">
+                <img src="" alt="">
+            </div>
+        </div>
+    </div> --}}
 
+    services page design here
 
 @endsection
